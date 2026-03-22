b8d1d9cdcc33f4c539e5b83c17f0d74c0475de0f
trainer2-computhink <trainer2@computhink.com.sg>
2026-03-22 04:42:42 +0000

    Task List 1

--- Student_Template/CT08_06/project6_grading.py
+++ Student_Template/CT08_06/project6_grading.py
@@ -1,8 +1,76 @@
-# Predefined data
-answer_key = ["A", "B", "B", "D"]  # Correct answers for the quiz
+answer_key = ["A", "B", "B", "D"]  
 student_answers = {
     "john": ["A", "C", "B", "D"],
     "jane": ["A", "B", "B", "D"],
     "alice": ["A", "C", "C", "D"],
     "bob": ["A", "B", "B", "D"]
 }
+
+def grade_all_students(student_answers, answer_key):
+    quiz_scores = {}
+    for name, answers in student_answers.items():
+        temp_score = 0
+        for i in range(len(answer_key)):
+            if answers[i] == answer_key[i]:
+                temp_score += 1
+        quiz_scores[name] = temp_score/len(answer_key) * 100
+    return quiz_scores
+quiz_scores = grade_all_students(student_answers, answer_key)
+print(quiz_scores)
+
+def calculate_average_score(quiz_scores):
+    sum = 0
+    for _, score in quiz_scores.items():
+        sum += score
+    return sum/len(quiz_scores)
+def find_highest_scorer(quiz_scores):
+    highest_scorers = []
+    max_score = 0
+    for _, score in quiz_scores.items():
+        if score > max_score:
+            max_score = score
+    for name, score in quiz_scores.items():
+        if score == max_score:
+           highest_scorers.append(name)
+    return highest_scorers
+def display_results(quiz_scores):
+    for name, score in quiz_scores.items():
+        print(f"{name} : {score}")
+display_results(quiz_scores)
+
+def menu_system():
+    choice = ""
+    quiz_scores = {}
+
+    while True:
+        print("Quiz Grading System Mneu")
+        print("1. Grade all Students")
+        print("2. Calculate Class Average")
+        print("3. Find Highest Scorer")
+        print("4. Display All Results")
+        print("5. Exit")
+        choice = int(input("Enter your choice: "))
+    
+        if choice == 1:
+            quiz_scores = grade_all_students(student_answers, answer_key)
+        elif choice == 2:
+            if len(quiz_scores) == 0:
+                print("Please grade all students first.")
+            else:
+                class_average = calculate_average_score(quiz_scores)
+                print(f"The class average is {class_average}")
+        elif choice == 3:
+            if len(quiz_scores) == 0:
+                print("Please grade all students first:")
+            else:
+                highest_scorer = find_highest_scorer(quiz_scores)
+                print("The highest scorer is:")
+                for scorer in highest_scorer:
+                    print(scorer)
+            
+        elif choice == 4:
+            display_results(quiz_scores)
+        else:
+            break
+
+menu_system()
---
+++ Student_Template/CT08_07/lesson7.py
@@ -0,0 +1,72 @@
+import os
+
+filepath = os.getcwd()
+fullpath = os.path.join(filepath, "tasks.txt")
+
+def create_task_file():
+    if os.path.exists(fullpath):
+        overwrite = input(f"{filepath} exists. Do you want to overwrite it? y/n: ")
+        if overwrite.lower() == "y":
+            with open(fullpath, 'w') as file:
+                file.write("My Task List\n")
+        else:
+            print("File not overwritten.")
+    else:
+        with open(fullpath, 'w') as file:
+            file.write("My Task List")
+    return
+        
+
+def view_all_tasks():
+    with open(fullpath, 'r') as file:
+        lines = file.readlines()
+        if len(lines) < 2:
+            print("No tasks found.")
+            return
+        for i in range(len(lines)):
+            if i == 0:
+                print(lines[i].strip())
+            else:
+                print(f"{i}. {lines[i].strip()}")
+
+def add_new_task():
+    print("Enter a new task: ")
+    task = input()
+    with open(fullpath, 'a') as file:
+        file.write(task)
+    print('\nTask added successfully')
+    return
+
+def delete_task():
+    pass
+
+def mark_task_done():
+    pass
+
+def display_menu():
+    print("Menu:")
+    print("1. Create a new task file")
+    print("2. View all tasks")
+    print("3. Add a new task")
+    print("4. Delete a task")
+    print("5. Mark a task as done")
+    print("6. Exit")
+    print("\nEnter your choice:")
+
+while True:
+    display_menu()
+    choice = input()
+    if choice == '1':
+        create_task_file()
+    elif choice == '2':
+        view_all_tasks()
+    elif choice == '3':
+        add_new_task()
+    elif choice == '4':
+        delete_task()
+    elif choice == '5':
+        mark_task_done()
+    elif choice == '6':
+        break
+    else:
+        choice = input("Enter a valid input from 1-6: ")
